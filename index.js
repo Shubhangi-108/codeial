1aceaebd8392bb8c46f8797f96da411bd254cb31
Shubhangi <shubhangi.108om@gmail.com>
2024-01-10 23:43:03 +0530

    add view engine

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 
 app.use('/' , require('./routes/index'));
 
+//set up the view engine
+app.set('view engine' , 'ejs')
+app.set('views' , './views');
+
 app.listen(port , function(err){
     if(err){
         // console.log('Error' , err);
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller')
 
 router.get('/' , homeController.home);
+router.use('/users' , require('./user'))
 module.exports = router;
