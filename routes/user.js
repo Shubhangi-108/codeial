c42d3f6927cf98a42ffa22d077286b1818ea34c2
Shubhangi-108 <shubhi108shivoham@gmail.com>
2024-01-11 19:04:58 +0530

    create sign in and sign up page

--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/users_controller')
 
 router.get('/profile' , userController.profile);
+
+router.get('/sign-up' , userController.signUp);
+router.get('/sign-in' , userController.signIn);
+
+
 module.exports = router;
