75d2ff6496ffe6af9cdb9aecac6711c02c56541d
Shubhangi <shubhangi.108om@gmail.com>
2024-01-11 20:21:00 +0530

    connecting to db

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
-//use express router
+app.use(express.static('./assets'));
 
-app.use('/' , require('./routes/index'));
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
-//set up the view engine
-app.set('view engine' , 'ejs')
-app.set('views' , './views');
 
-app.listen(port , function(err){
-    if(err){
-        // console.log('Error' , err);
-        console.log(`Error in running thr server: ${err}`);
+// use express router
+app.use('/', require('./routes'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
     }
-    console.log(`Server is running om port : ${port}` );
-})
+
+    console.log(`Server is running on port: ${port}`);
+});
