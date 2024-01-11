cd766dee6aae47d40751e65343801251bd9e10f5
Shubhangi <shubhangi.108om@gmail.com>
2024-01-11 19:04:58 +0530

    create sign in and sign up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,29 @@
 module.exports.profile = function(req , res){
-    return res.end('<h1>User Profile</h1>');
+    return res.render('user' , {
+        title: "profile"
+    })
+}
+
+//render the sign up part
+module.exports.signUp = function(req ,res){
+    return res.render('user_signup' , {
+        title: "Codeial|Sign Up"
+    })
+}
+
+//render the sign in page
+module.exports.signIn = function(req ,res){
+    return res.render('user_signin' , {
+        title: "Codeial|Sign In"
+    })
+}
+
+//get the sign up data
+module.exports.create = function(req , res){
+    //TODO later
+}
+
+// sign in and create a session for the user
+module.exports.createSession = function(req , res){
+    //TODO later
 }
