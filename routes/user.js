9b4364f28c9115c605c0cffcddc2f2dca7307256
Shubhangi <shubhangi.108om@gmail.com>
2024-01-12 14:39:53 +0530

    create db for sign-up

--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,7 @@ router.get('/profile' , userController.profile);
 router.get('/sign-up' , userController.signUp);
 router.get('/sign-in' , userController.signIn);
 
+router.post('/create' , userController.create);
+
 
 module.exports = router;
