1713b5a3e661d040054d06fa26f66b67448371de
Shubhangi-108 <shubhi108shivoham@gmail.com>
2024-01-14 00:39:55 +0530

    created the signin signout page with passport.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+//used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passpostLocal = require('./config/passport-local-stategy');
+const MongoStore = require('connect-mongo');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
@@ -16,13 +21,38 @@ app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
 
-// use express router
-app.use('/', require('./routes'));
-
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+//mongo store is used to store the seeion cookie in the db
+app.use(session ({
+    name: 'codeial', //name of the cookie
+    //TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie:{
+        maxAge: (1000*60*100)
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl: 'mongodb://localhost/codeial_development',
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongodb set up');
+        }
+    )
+}))
+
+app.use( passport.initialize ());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if (err){
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const userController = require('../controllers/users_controller')
 
-router.get('/profile' , userController.profile);
+router.get('/profile' , passport.checkAuthentication, userController.profile);
 
 router.get('/sign-up' , userController.signUp);
 router.get('/sign-in' , userController.signIn);
 
 router.post('/create' , userController.create);
 
+//use passport as a middleware to authenticate
+router.post('/create-session' , passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'},
+
+), userController.createSession)
+
+router.get('/sign-out' ,userController.distroySession);
 
 module.exports = router;
