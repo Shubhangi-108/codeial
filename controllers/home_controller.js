976a6fff8b84e21a889178f3cf0943b5b8a27283
Shubhangi <shubhangi.108om@gmail.com>
2024-01-22 21:59:16 +0530

    create a post and dispay it in view and which user posted it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,18 @@
-module.exports.home = function(req , res){
-    console.log(req.cookies);
-    return res.render('home', {
-        title: 'Home'
-    })
+const Post = require( '../models/post');
+
+module.exports.home = async function(req , res){
+    // console.log(req.cookies);
+    try {
+        // Use async/await to handle asynchronous operation
+        const posts = await Post.find({}).populate('user').exec();
+
+        return res.render('home', {
+            title: 'Codeial | Home',
+            posts: posts
+        });
+    } catch (err) {
+        console.error('Error in fetching posts:', err);
+        return res.redirect('back');
+    }
+
 }
