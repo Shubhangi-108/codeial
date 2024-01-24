88e9e234874b2ce4f85655047379022329803dc4
Shubhangi-108 <shubhi108shivoham@gmail.com>
2024-01-24 15:09:10 +0530

    now you can delete the post and comments

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -36,3 +36,44 @@ module.exports.create = function (req, res) {
             return res.redirect('back');
         });
 };
+
+// module.exports.create = function( req , res ){
+//     Post.create({
+//         content: req.body.content,
+//         user: req.user._id
+//     })
+//     .then(post => {
+//         console.log('Post created successfully:', post);
+//         return res.redirect('back');
+//     })
+//     .catch(err => {
+//         console.error('Error in creating a post:', err);
+//         return res.redirect('back');
+//     });
+// }
+
+module.exports.destroy = async function (req, res) {
+    try {
+        const comment = await Comment.findById(req.params.id);
+
+        if (!comment) {
+            return res.status(404).send("Comment not found");
+        }
+
+        // Check if the user is the owner of the comment
+        if (comment.user.toString() === req.user.id.toString()) {
+            const postId = comment.post;
+
+            await comment.deleteOne();
+
+            await Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } });
+
+            return res.redirect('back');
+        } else {
+            return res.status(403).send("Unauthorized");
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send("Internal Server Error");
+    }
+};
--- controllers/posts_contoller.js
+++ controllers/posts_contoller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function( req , res ){
     Post.create({
@@ -14,3 +15,28 @@ module.exports.create = function( req , res ){
         return res.redirect('back');
     });
 }
+
+module.exports.destroy = async function (req, res) {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).send("Post not found");
+        }
+
+        // Check if the user is the owner of the post
+        if (post.user == req.user.id) {
+             await post.deleteOne();
+
+            // Remove comments associated with the post
+            await Comment.deleteMany({ post: req.params.id });
+
+            return res.redirect('back');
+        } else {
+            return res.status(403).send("Unauthorized");
+        }
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send("Internal Server Error");
+    }
+};
