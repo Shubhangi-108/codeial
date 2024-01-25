1d649970f0f12a1d713ea20fe7cbad7ad89a7867
Shubhangi <shubhangi.108om@gmail.com>
2024-01-25 21:57:08 +0530

    posting via ajax

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -7,6 +7,7 @@ module.exports.home = async function(req , res){
     try {
         // Use async/await to handle asynchronous operation
         const posts = await Post.find({})
+        .sort('-createdAt')
         .populate('user')
         .populate({
             path: 'comments',
--- controllers/posts_contoller.js
+++ controllers/posts_contoller.js
@@ -17,19 +17,27 @@ const Comment = require('../models/comment');
 // }
 
 module.exports.create = async function( req , res ){
-
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+
+        if( req.xhr ){
+            return res.status(200).json({
+                data:{
+                    post: post
+                },
+                message: "Post created!!"
+            })
+        }
 
         req.flash('success' , 'Post published!')
         return res.redirect('back');
     }
     catch(err){
         req.flash('error' , err);
-        return;
+        return res.redirect('back');
     }
 }
 
@@ -73,6 +81,16 @@ module.exports.destroy = async function (req, res) {
 
             // Remove comments associated with the post
             await Comment.deleteMany({ post: req.params.id });
+
+            if( req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    }, 
+                    message: 'Post deleted'
+                })
+            }
+
             req.flash('success' , 'Post and assocoated comments deleted!')
             return res.redirect('back');
         } else {
@@ -82,6 +100,6 @@ module.exports.destroy = async function (req, res) {
     } catch (err) {
 
         req.flash('error' , err)
-        return res.status(500).send("Internal Server Error");
+        return res.redirect('back');
     }
 };
